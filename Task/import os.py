4dd42ba59984270b925dc8697868cc3b9b3fbc9b
Nikita Yalov <wolfox.nikita@gmail.com>
2024-03-25 13:32:01 +0400

    Piks

--- Task/import os.py
+++ Task/import os.py
@@ -3,19 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Инициализация массивов для хранения данных
-average_spectrum = np.zeros(100)
+kika = 200
+average_spectrum = np.zeros(kika)
 
 folder_path = '2023-04-07_SERS_plasma_control_cyst'
 spectra_files = os.listdir(folder_path)
 
 all_spectra = []
-step = 2
+step = 1
 # Объединение данных всех спектров и получение минимального и максимального спектра
 for file in spectra_files:
     data = np.loadtxt(os.path.join(folder_path, file))
 
     if data.shape[0] != 1000:
-        data = np.interp(np.linspace(0, 1, 100), np.linspace(0, 1, data.shape[0]), data[:, 1])
+        data = np.interp(np.linspace(0, 1, kika), np.linspace(0, 1, data.shape[0]), data[:, 1])
 
     all_spectra.append(data)
     average_spectrum += data
@@ -39,6 +40,7 @@ for i in range(0, len(average_spectrum), step):
 
     plt.plot([i, i], [min_val, max_val], color='gray', linestyle='-',
              linewidth=1)  # Соединение минимальной и максимальной точек серой тонкой линией
+
 
 plt.legend()
 plt.show()
